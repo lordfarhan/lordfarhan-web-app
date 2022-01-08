ea244a1dfd4a3bddecb5c5bf77d9f52b5245908a
Muhammad Farhan Abdur Rosyid <farhan1205muhammad@gmail.com>
2022-01-08 13:42:06 +0700

    Fix icon and configuration.

--- gatsby-config.plugins.js
+++ gatsby-config.plugins.js
@@ -16,8 +16,8 @@ module.exports = [
   {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      name: 'Rolwin Reevan',
-      short_name: 'Rolwin Reevan',
+      name: 'Lord Farhan',
+      short_name: 'Lord Farhan',
       start_url: '/',
       background_color: '#304CFD',
       theme_color: '#304CFD',
